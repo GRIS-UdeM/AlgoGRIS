8a145a5c32d39d82a2ec788286a605268c4779f6
Vincent Berthiaume <vberthiaume@gmail.com>
2025-04-11 11:57:57 -0400

    cmake

---
+++ tests/tests.cpp
@@ -0,0 +1,12 @@
+#include "../sg_AbstractSpatAlgorithm.hpp"
+
+/**
+ * @brief Main test function.
+*/
+int main ()
+{
+    juce::UnitTestRunner testRunner;
+    testRunner.runAllTests();
+
+    return 0;
+}
