f142996948971f53821e4b1dab1f29a7363369b8
Vincent Berthiaume <vberthiaume@gmail.com>
2025-07-21 15:53:49 -0400

    clangformat

--- tests/unit/test_spatAlgorithms.cpp
+++ tests/unit/test_spatAlgorithms.cpp
@@ -373,9 +373,8 @@ TEST_CASE(vbapTestName, "[spat]")
                          sourcePeaks);
     std::cout << vbapTestName << " tests done." << std::endl;
 
-
     // 3. benchmarks, using more sources
-    vbapData = getSpatGrisDataFromFiles ("default_preset_256.xml", "default_speaker_setup.xml");
+    vbapData = getSpatGrisDataFromFiles("default_preset_256.xml", "default_speaker_setup.xml");
     benchmarkUsingProjectData(vbapData,
                               sourceBuffer,
                               speakerBuffer,
