307031f18e34fce7b045a7842d93f4e802d3bfee
Vincent Berthiaume <vberthiaume@gmail.com>
2025-08-12 15:08:48 -0400

    building

--- tests/unit/test_speaker_setup_conversion.cpp
+++ tests/unit/test_speaker_setup_conversion.cpp
@@ -1,7 +1,7 @@
 #include "../sg_TestUtils.hpp"
 #include <Data/sg_LegacySpatFileFormat.hpp>
+#include <Utilities/ValueTreeUtilities.hpp>
 #include <catch2/catch_all.hpp>
-#include <UtilitiesValueTreeUtilities.hpp>
 
 using namespace gris;
 using namespace gris::tests;
