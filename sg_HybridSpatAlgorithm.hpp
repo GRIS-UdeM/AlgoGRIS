8a2591e86b1523f3ade3b95c100078bfe161ba4b
Guillaume Riou <117857232+guillaumeriousat@users.noreply.github.com>
2025-11-03 09:13:48 -0500

    Improve multicore DSP performance by implementing a spin/sleep strategy (#30)
    Note: This points on the `main-dev` branch of fork_union, where the changes I contributed to fork_union to make this work are at the moment.
    
    A version 3.0 of fork_union is in the works but there are minor API compatibilities that will need to be ironed out to use it in algoGRIS.

--- sg_HybridSpatAlgorithm.hpp
+++ sg_HybridSpatAlgorithm.hpp
@@ -26,6 +26,11 @@ namespace gris
 /** A spatialization algorithm that uses both Vbap (dome) and Mbap (cube).
  *
  * The selection of the algorithm is done on a per-source basis.
+ *
+ * Note: This class is templated to make it possible to use different implementation of
+ * the VBAP and MBAP spatialization. The use case right now is to use
+ * ParallelVbapSpatAlgorithm and ParallelMbapSpatAlgorithm but benchmarking has shown
+ * its not better than single threaded for the hybrid use-case.
  */
 template<typename MBAP, typename VBAP>
 class HybridSpatAlgorithm final : public AbstractSpatAlgorithm
@@ -50,8 +55,8 @@ public:
     {
         if (!sourceData.position.has_value()) {
             // resetting a position should reset both algorithms
-            mVbap->updateSpatData(sourceIndex, sourceData);
             mMbap->updateSpatData(sourceIndex, sourceData);
+            mVbap->updateSpatData(sourceIndex, sourceData);
             return;
         }
 
@@ -77,8 +82,8 @@ public:
                  SourcePeaks const & sourcePeaks,
                  SpeakersAudioConfig const * altSpeakerConfig) noexcept override
     {
-        mVbap->process(config, sourcesBuffer, speakersBuffer, stereoBuffer, sourcePeaks, altSpeakerConfig);
         mMbap->process(config, sourcesBuffer, speakersBuffer, stereoBuffer, sourcePeaks, altSpeakerConfig);
+        mVbap->process(config, sourcesBuffer, speakersBuffer, stereoBuffer, sourcePeaks, altSpeakerConfig);
     }
 
     juce::Array<Triplet> getTriplets() const noexcept override { return mVbap->getTriplets(); }
